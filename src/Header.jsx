a4f1e43e48ad297d4b7ea784e432939de9031093
Zack <168221270+Zval321@users.noreply.github.com>
2024-12-01 15:16:42 -0600

    updated for mobile use

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,44 +1,63 @@
 import './Header.css';
-import { NavLink, useLocation } from 'react-router-dom'
+import { NavLink, useLocation } from 'react-router-dom';
 
 export function Header() {
-  const location = useLocation()
+  const location = useLocation();
+
   return (
-    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        <NavLink to={location.pathname === '/MyInterests' ? '/' : "/"} className='logo'>ZV</NavLink>
+        {/* Logo */}
+        <NavLink to={location.pathname === '/MyInterests' ? '/' : '/'} className="logo">
+          ZV
+        </NavLink>
+
+        {/* Hamburger Button for Mobile */}
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Navbar Links */}
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <NavLink to={location.pathname === '/Myinterests' ? "/" : '/'} className={({ isActive }) => (isActive ? "nav-link active-link" : "nav-link")}>
+              <NavLink 
+                to={location.pathname === '/MyInterests' ? '/' : '/'} 
+                className={({ isActive }) => (isActive ? 'nav-link active-link' : 'nav-link')}
+              >
                 Home
               </NavLink>
             </li>
+
+            {/* Conditional Links */}
             {location.pathname !== '/MyInterests' && (
-            <li className="nav-item">
-              <a href='#projects' className='nav-link'>
-                My Projects
-              </a>
-            </li>
+              <li className="nav-item">
+                <a href="#projects" className="nav-link">
+                  My Projects
+                </a>
+              </li>
             )}
-            {location.pathname !== '/MyInterests' && ( 
-            <li className="nav-item">
-              <a href='#about' className='nav-link'>
-                About Me
-              </a>
-            </li>
+            {location.pathname !== '/MyInterests' && (
+              <li className="nav-item">
+                <a href="#about" className="nav-link">
+                  About Me
+                </a>
+              </li>
             )}
-            <li className='nav-item'>
-              <NavLink to='/MyInterests' className={({ isActive }) => (isActive ? "nav-link active-link" : "nav-link")}>
-              My Interests
+            <li className="nav-item">
+              <NavLink 
+                to="/MyInterests" 
+                className={({ isActive }) => (isActive ? 'nav-link active-link' : 'nav-link')}
+              >
+                My Interests
               </NavLink>
             </li>
-            {location.pathname !== '/MyInterests' && ( 
-            <li className="nav-item">
-              <a href="#contact" className='nav-link'>
-                Contact
-              </a>
-            </li>
+            {location.pathname !== '/MyInterests' && (
+              <li className="nav-item">
+                <a href="#contact" className="nav-link">
+                  Contact
+                </a>
+              </li>
             )}
           </ul>
         </div>
