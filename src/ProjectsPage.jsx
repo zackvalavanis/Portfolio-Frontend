b145030118a0413bcb017adf0dc372dccaacc666
Zack <168221270+Zval321@users.noreply.github.com>
2025-01-14 19:08:28 -0600

    removed project

--- src/ProjectsPage.jsx
+++ src/ProjectsPage.jsx
@@ -7,36 +7,8 @@ export function ProjectsPage() {
       
       {/* Link to the deployed project */}
       <div className="project-item">
-        <h2>Calendar</h2>
-        <p>Check out my latest project:</p>
-        <div className='linked-items'>
-        <a 
-          href="https://capstone-to-do-frontend-a91vy1orq-zackvalavanis-projects.vercel.app/" 
-          target="_blank" 
-          rel="noopener noreferrer"
-        >
-          Website
-        </a>
-        <a 
-          href='https://github.com/zackvalavanis/Capstone_To_Do'
-          target='blank'
-          rel="noopener noreferrer"
-        >
-          Backend Code
-        </a>
-        <a
-          href='https://github.com/zackvalavanis/To_Do_Frontend'
-          target='blank'
-          rel="noopener noreferrer"
-        >
-          Frontend Code
-        </a>
-        {/* Project Preview - Image */}
-        <img 
-          src="/Images/Calendar.jpg" /* Ensure the path is correct */
-          alt="Calendar Project Preview"
-        />
-        </div>
+        <h2></h2>
+        <p>Projects Coming Soon...</p>
       </div>
     </div>
   );
