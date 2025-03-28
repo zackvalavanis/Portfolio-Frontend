90d6018d050000102fef6919191ea2e8d9294dcd
Zack <168221270+Zval321@users.noreply.github.com>
2025-03-28 13:45:20 -0500

    added first project to the protfolio

--- src/ProjectsPage.jsx
+++ src/ProjectsPage.jsx
@@ -8,7 +8,7 @@ export function ProjectsPage() {
       
       {/* Link to the deployed project */}
       <div className="project-item">
-        <h2 className='header-project-zen'>Zen Rolls Sushi Website</h2>
+        <h2 className='header-project-zen'>Zen Rolls</h2>
         <Link 
           to='https://zen-rolls-flm56gin3-zackvalavanis-projects.vercel.app/'>
           <img 
@@ -17,10 +17,10 @@ export function ProjectsPage() {
           </img>
         </Link>
         <p>
-          <strong>Zen Rolls</strong> is a restaurant-themed website I built to showcase my web development skills.  
+          <strong>Zen Rolls</strong> A restaurant-themed website built with a Ruby on Rails backend and a React, JavaScript, and TypeScript frontend. The app allows users to experience a fully developed website for a mock sushi restaurant, featuring a Stripe integration that simulates order placement. Users can browse a dynamic menu, add items to their cart, and go through a seamless checkout experience. The app also incorporates responsive design for mobile compatibility and interactive UI elements to enhance the user experience.
         </p>  
-        <p><strong>Tech Stack:</strong></p>  
-        <ul>  
+        <p className='title-zen'><strong>Tech Stack:</strong></p>  
+        <ul className='list'>  
           <li><strong>Backend:</strong> Ruby on Rails</li>  
           <li><strong>Frontend:</strong> JavaScript, TypeScript, React</li>  
           <li><strong>Integrations:</strong> Stripe</li>  
