bb8083e83d03ca360915b196ced83105b9f6eabe
Zack <168221270+Zval321@users.noreply.github.com>
2025-03-28 11:37:48 -0500

    added zen rolls to the project section of my application

--- src/ProjectsPage.jsx
+++ src/ProjectsPage.jsx
@@ -1,4 +1,5 @@
 import './ProjectsPage.css';
+import { Link } from 'react-router-dom'
 
 export function ProjectsPage() {
   return (
@@ -7,8 +8,23 @@ export function ProjectsPage() {
       
       {/* Link to the deployed project */}
       <div className="project-item">
-        <h2></h2>
-        <p>Projects Coming Soon...</p>
+        <h2 className='header-project-zen'>Zen Rolls Sushi Website</h2>
+        <Link 
+          to='https://zen-rolls-flm56gin3-zackvalavanis-projects.vercel.app/'>
+          <img 
+            className='image-zen'
+            src='/Images/Screenshot 2025-03-28 at 11.33.05 AM.png'>
+          </img>
+        </Link>
+        <p>
+          <strong>Zen Rolls</strong> is a restaurant-themed website I built to showcase my web development skills.  
+        </p>  
+        <p><strong>Tech Stack:</strong></p>  
+        <ul>  
+          <li><strong>Backend:</strong> Ruby on Rails</li>  
+          <li><strong>Frontend:</strong> JavaScript, TypeScript, React</li>  
+          <li><strong>Integrations:</strong> Stripe</li>  
+        </ul>  
       </div>
     </div>
   );
