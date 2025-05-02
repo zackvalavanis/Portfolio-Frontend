a77ad962fb44c0733d1f76924b8aed8defb5dff7
Zack <168221270+Zval321@users.noreply.github.com>
2025-05-02 09:52:39 -0500

    added second project to the portfolio

--- src/ProjectsPage.jsx
+++ src/ProjectsPage.jsx
@@ -3,33 +3,58 @@ import { Link } from 'react-router-dom'
 
 export function ProjectsPage() {
   return (
-    <div className="projects">
-      <h1>My Projects</h1>
-      
-      {/* Link to the deployed project */}
-      <div className="project-item">
-        <h2 className='header-project-zen'>Restaurant Website</h2>
-        <Link 
-          to='https://zen-rolls-flm56gin3-zackvalavanis-projects.vercel.app/'>
-          <img 
-            className='image-zen'
-            src='/Images/Screenshot 2025-03-28 at 11.33.05 AM.png'>
-          </img>
-        </Link>
-        <p>
-          <strong>Zen Rolls</strong> A restaurant-themed website built with a Ruby on Rails backend and a React, JavaScript, and TypeScript frontend. The app allows users to experience a fully developed website for a mock sushi restaurant, featuring a Stripe integration that simulates order placement. Users can browse a dynamic menu, add items to their cart, and go through a seamless checkout experience. The app also incorporates responsive design for mobile compatibility and interactive UI elements to enhance the user experience.
-        </p>  
-        <p className='title-zen'><strong>Tech Stack:</strong></p>  
-        <ul className='list'>  
-          <li><strong>Backend:</strong> Ruby on Rails</li>  
-          <li><strong>Frontend:</strong> JavaScript, TypeScript, React</li>  
-          <li><strong>Integrations:</strong> Stripe</li>  
-        </ul>  
-        <div className='frontend-backend'>
-            <a className='frontend' href='https://github.com/zackvalavanis/zenRolls-Frontend'>Frontend Code</a>
-            <a className='backend' href='https://github.com/zackvalavanis/Zen_Rolls_API'>Backend Code</a>
+    <div className='whole-page'>
+      <h1 className='title-1'>My Projects</h1>
+
+      <div className="projects">
+        {/* Flex container for project cards */}
+          <div className="project-item">
+            <h2 className='header-project-zen'>Restaurant Website</h2>
+            <Link to='https://zen-rolls-flm56gin3-zackvalavanis-projects.vercel.app/'>
+              <img
+                className='image-small'
+                src='/Images/Screenshot 2025-03-28 at 11.33.05 AM.png'
+                alt='Zen Rolls'
+              />
+            </Link>
+            <p>
+              <strong>Zen Rolls</strong> A restaurant-themed website built with a Ruby on Rails backend and a React, JavaScript, and TypeScript frontend...
+            </p>
+            <p className='title-zen'><strong>Tech Stack:</strong></p>
+            <ul className='list'>
+              <li><strong>Backend:</strong> Ruby on Rails</li>
+              <li><strong>Frontend:</strong> JavaScript, TypeScript, React</li>
+              <li><strong>Integrations:</strong> Stripe</li>
+            </ul>
+            <div className='frontend-backend'>
+              <a className='frontend' href='https://github.com/zackvalavanis/zenRolls-Frontend'>Frontend Code</a>
+              <a className='backend' href='https://github.com/zackvalavanis/Zen_Rolls_API'>Backend Code</a>
+            </div>
+          </div>
+
+          <div className="project-item">
+            <h2 className='header-project-2'>HTML & CSS</h2>
+            <Link to='https://zackvalavanis.github.io/Webpage-Project/'>
+              <img
+                className='image-small'
+                src='/Images/Screenshot 2025-05-02 at 9.24.51 AM.png'
+                alt='web-design-image'
+              />
+            </Link>
+            <p>
+              <strong>Zen Rolls</strong> A restaurant-themed website built with a Ruby on Rails backend and a React, JavaScript, and TypeScript frontend...
+            </p>
+            <p className='title-zen'><strong>Tech Stack:</strong></p>
+            <ul className='list'>
+              <li><strong>Backend:</strong> N/A</li>
+              <li><strong>Frontend:</strong> HTML and CSS       </li>
+              <li><strong>Integrations:</strong> N/A</li>
+            </ul>
+            <div className='frontend-backend'>
+              <a className='frontend' href='https://github.com/zackvalavanis/zenRolls-Frontend'>Frontend Code</a>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
   );
 }
